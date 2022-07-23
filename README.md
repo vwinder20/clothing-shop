d4debcd141fda8f6138efe09344d12e08150bec1
zskio <vwind5224@gmail.com>
2022-07-23 13:28:55 +0400

    readme added

---
+++ README.md
@@ -0,0 +1,7 @@
+# TECH STACK
+
+- React JS
+- Tailwind
+- Vite
+
+For testing server(db.json), use json-server.
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="categories-container">
+      <div className="category-container"></div>
     </div>
   );
-}
+};
 
 export default App;
