47d062d7dce4ca5e5a3d48639999d1f546e7d43d
zskio <vwind5224@gmail.com>
2022-07-28 18:06:53 +0400

    clean up and complete this sing in form

--- src/routes/SingIn/SingIn.jsx
+++ src/routes/SingIn/SingIn.jsx
@@ -1,18 +1,22 @@
-import { async } from "@firebase/util";
 import React from "react";
 import {
   signInWithPopupFunc,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase";
+import SignUp from "../../components/SignUp";
+import { useState } from "react";
+
 const SingIn = () => {
   const logGoogleUser = async () => {
     const { user } = await signInWithPopupFunc();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
+
   return (
     <div>
       <h1>Sing In page</h1>
       <button onClick={logGoogleUser}>Sign In with Google</button>
+      <SignUp />
     </div>
   );
 };
